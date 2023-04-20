6f5daada73c07b31cdbfdd4d1068bbe41166cc9f
Son <rooneei@naver.com>
2023-04-20 23:22:50 +0900

    bfs,dfs..

---
+++ ProgrammersJava/src/BFSDFS_백준_숨바꼭질.java
@@ -0,0 +1,56 @@
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class BFSDFS_백준_숨바꼭질 {
+    static int N,M;
+    static int counter[] = new int[100001]; // 방문배열 겸, count 세는 counter
+    static int[] dx = {1,-1,2};
+//    static boolean visited[];
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt(); // 현재 위치
+        M = in.nextInt(); // 동생 위치
+        counter[N] = 0;
+
+        // 서로 처음부터 똑같은 예외상황 처리
+        if (N == M){
+            System.out.println(0);
+        } else{
+            bfs(N);
+            System.out.println(counter[M]);
+        }
+    }
+
+    private static void bfs(int current){
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(current);
+        // 방문배열 있을 자리 (처음 들어가는 숫자 방문배열 true 시켜주기)
+
+        while(!que.isEmpty()){
+            int now = que.poll();
+
+            if(now == M) return;
+
+            for(int i=0; i<3;i++){
+                // 다음 가놓을 곳 미리 지정
+                int next;
+                if (i==2) {next = now*dx[i]; }
+                else next = now+dx[i];
+
+                if(next>=0 && next<=100000){ // 1. 범위벗어나지 않고
+                    if(counter[next] == 0){ // 2. 방문배열에서 방문하지 않은 곳일 때,
+                        // 만약 방문했을 때, 방문배열 true 시켜주는 자리 -> 여기서는 counter == 0 으로 대신 검사 가능
+
+                        // 다음 방문 확정
+                        counter[next] = counter[now] + 1;
+                        que.add(next);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
--- ProgrammersJava/src/test11.java
+++ ProgrammersJava/src/test11.java
@@ -3,21 +3,7 @@ import java.util.*;
 
 public class test11 {
     public static void main(String[] args) {
-        ArrayList<int[]> list = new ArrayList<>();
-        int [] A = new int[3];
-        int [] B = new int[3];
-
-        for (int i =0;i<3; i ++){
-            A[i] = i;
-            B[i] = i+1;
-        }
-        list.add(A);
-        list.add(B);
-        for (int[] i : list){
-            System.out.println(Arrays.toString(i));
-        }
-
-
-
+        int a = Integer.MAX_VALUE;
+        System.out.println(a);
     }
 }
